fda22de31a4dc92d9e0b80dc7208d58c2ec0c7e3
Zifan Pan <brianpanzifan@gmail.com>
2023-02-05 03:41:45 -0500

    dynamically add href upon loading

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-import * as urls from "./assets/urls/urls";
+import * as urls from "./assets/urls/urls.js";
+
+const personalProjectNames = [
+  "dishGuru",
+  "tripPlanner",
+  "rockPaperScissorsGame",
+  "roomPage",
+  "roadSafetyMachineLearningReport",
+  "fuelEfficiencyStatisticalModelingProject",
+];
+const workProjectNames = [
+  "medicationLocator",
+  "conditionFinder",
+  "bmiCalculator",
+  "featureFilter",
+  "milestones",
+];
+for (let name of personalProjectNames) {
+  if (document.querySelector(`#${name}LiveLink`)) {
+    document
+      .querySelector(`#${name}LiveLink`)
+      .setAttribute("href", urls[`${name}Live`]);
+    document
+      .querySelector(`#${name}GitHubLink`)
+      .setAttribute("href", urls[`${name}GitHub`]);
+  }
+}
+for (let name of workProjectNames) {
+  if (document.querySelector(`#${name}LiveLink`)) {
+    document
+      .querySelector(`#${name}LiveLink`)
+      .setAttribute("href", urls[`${name}Live`]);
+  }
+}
