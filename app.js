c7073bd3720f8a9937e4c0c5f9d46e4bfe60529b
Zifan Pan <brianpanzifan@gmail.com>
2023-02-06 16:14:23 -0500

    refactored code for name listing

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
+import { personalProjectNames, workProjectNames } from "./data/names/names.js";
 import * as urls from "./data/urls/urls.js";
 
-const personalProjectNames = [
-  "dishGuru",
-  "tripPlanner",
-  "rockPaperScissorsGame",
-  "roomPage",
-  "roadSafetyMachineLearningReport",
-  "fuelEfficiencyStatisticalModelingProject",
-];
-const workProjectNames = [
-  "medicationLocator",
-  "conditionFinder",
-  "bmiCalculator",
-  "featureFilter",
-  "milestones",
-];
 for (let name of personalProjectNames.concat(workProjectNames)) {
   document
     .querySelector(`#${name}LiveLink`)
---
+++ data/names/names.js
@@ -0,0 +1,15 @@
+export const personalProjectNames = [
+  "dishGuru",
+  "tripPlanner",
+  "rockPaperScissorsGame",
+  "roomPage",
+  "roadSafetyMachineLearningReport",
+  "fuelEfficiencyStatisticalModelingProject",
+];
+export const workProjectNames = [
+  "medicationLocator",
+  "conditionFinder",
+  "bmiCalculator",
+  "featureFilter",
+  "milestones",
+];
