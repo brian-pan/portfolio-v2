af896d466b685a91002894e00135e0815c538b8e
Zifan Pan <brianpanzifan@gmail.com>
2023-02-06 16:15:46 -0500

    seperatedproject project descriptions and restructured paragraph to bullet points

---
+++ data/descriptions/descriptions.js
@@ -0,0 +1,8 @@
+//DESCRIPTION BULLET POINTS
+//dish guru
+export const dishGuruDescriptions = [
+  "A self-built full-stack (MERN) application with MongoDB, Express, React, and NodeJS.",
+  "Users are able to explore various recipes created by other users and write their reviews for these recipes.",
+  "An user can view, edit, and write their own recipes after register/login.",
+  "All data including users, dish recipes, and the associated reviews are processed and passed using Redux and Express, and stored in MongoDB.",
+];
