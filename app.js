ac89fe6b02b7353197483d72f229808da9255ce3
Zifan Pan <brianpanzifan@gmail.com>
2023-02-07 03:34:49 -0500

    added func to add camel cased name

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import { projects } from "./data/projects.js";
 
 console.log(projects);
+
+//add camelCasedName upon loading
+for (let project of projects.personal.concat(projects.work)) {
+  const words = project.name.split(" ");
+  words[0] = words[0].toLowerCase();
+  const camelCasedName = words.join("");
+  project.camelCasedName = camelCasedName;
+}
