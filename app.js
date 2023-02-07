e0058544f6343417f9d4790f0675cf360a1de260
Zifan Pan <brianpanzifan@gmail.com>
2023-02-07 04:14:38 -0500

    added func to generate heading

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import { projects } from "./data/projects.js";
-
 console.log(projects);
 
-//add camelCasedName upon loading
-for (let project of projects.personal.concat(projects.work)) {
+const allProjects = projects.personal.concat(projects.work);
+
+//add camelCasedName to each project upon loading
+for (let project of allProjects) {
   const words = project.name.split(" ");
   words[0] = words[0].toLowerCase();
   const camelCasedName = words.join("");
   project.camelCasedName = camelCasedName;
 }
+
+//create h3 for each project
+for (let project of allProjects) {
+  const h3 = document.createElement("h3");
+  h3.classList.add("project__heading");
+  h3.setAttribute("id", `${project.camelCasedName}Heading`);
+  h3.textContent = project.name;
+  document.querySelector(`#${project.camelCasedName}Wrapper`).appendChild(h3);
+}
+
+//
