376d02adbb28dec0aeb624a9a5ba871e0c2aaead
Zifan Pan <brianpanzifan@gmail.com>
2023-02-07 03:34:02 -0500

    restructured data to object oriented format

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-import { personalProjectNames, workProjectNames } from "./data/names/names.js";
-import * as urls from "./data/urls/urls.js";
+import { projects } from "./data/projects.js";
 
-for (let name of personalProjectNames.concat(workProjectNames)) {
-  document
-    .querySelector(`#${name}LiveLink`)
-    ?.setAttribute("href", urls[`${name}Live`]);
-  document
-    .querySelector(`#${name}GitHubLink`)
-    ?.setAttribute("href", urls[`${name}GitHub`]);
-}
+console.log(projects);
---
+++ data/projects.js
@@ -0,0 +1,26 @@
+export const projects = {
+  personal: [
+    {
+      name: "Dish Guru",
+      description:
+        "A self-built full-stack (MERN) application with MongoDB, Express, React, and NodeJS. Users are able to explore various recipes created by other users and write their reviews for these recipes. An user can view, edit, and write their own recipes after register/login. All data including users, dish recipes, and the associated reviews are processed and passed using Redux and Express, and stored in MongoDB.",
+      urlRepo: "https://github.com/brian-pan/dish-guru-app",
+      urlLive: "https://brian-dish-guru.onrender.com",
+    },
+    {
+      name: "Rock Paper Scissors Game",
+      description:
+        "A Rock-Paper-Scissor game that is won best out of three. The player will select a move, while the computer will randomly select their move using the GET api endpoint. Desktop Only with focuses on CSS",
+      urlRepo: "https://github.com/brian-pan/rock-paper-scissors-game",
+      urlLive: "https://brian-pan.github.io/rock-paper-scissors-game/",
+    },
+  ],
+  work: [
+    {
+      name: "Condition Finder",
+      description: "hello world",
+      urlLive:
+        "https://www.shoppersdrugmart.ca/en/health-and-wellness/conditions",
+    },
+  ],
+};
