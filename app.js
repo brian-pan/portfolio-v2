ac3b442a95e49113c3be8268ad210d790488c05f
Zifan-Pan <brianpanzifan@gmail.com>
2023-03-23 18:55:09 -0400

    simplified code syntax

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ console.log(projects);
 
 const allProjects = projects.personal.concat(projects.work);
 
-//add camelCasedName to each project upon loading
 for (let project of allProjects) {
+
+  //add camelCasedName to each project upon loading
   const words = project.name.split(" ");
   words[0] = words[0].toLowerCase();
   const camelCasedName = words.join("");
   project.camelCasedName = camelCasedName;
-}
 
-//create h3 for each project
-for (let project of allProjects) {
+  //create & append wrapper divs
+  const infoWrapper = document.createElement('div')
+  const descriptionWrapper = document.createElement('div')
+  document.querySelector(`#${project.camelCasedName}Wrapper`).appendChild(infoWrapper);
+  document.querySelector(`#${project.camelCasedName}Wrapper`).appendChild(descriptionWrapper);
+
+  //create h3 for each project
   const h3 = document.createElement("h3");
   h3.classList.add("project__heading");
   h3.setAttribute("id", `${project.camelCasedName}Heading`);
   h3.textContent = project.name;
-  document.querySelector(`#${project.camelCasedName}Wrapper`).appendChild(h3);
-}
+  infoWrapper.appendChild(h3);
 
 //add tech stacks for each project
-for (let project of allProjects) {
   const p = document.createElement("p");
   p.classList.add("project__tech-stacks");
   document.querySelector(`#${project.camelCasedName}Wrapper`).appendChild(p);
@@ -38,10 +41,9 @@ for (let project of allProjects) {
       p.appendChild(span).after(separator);
     }
   }
-}
+  infoWrapper.appendChild(p)
 
 //add urls to for each project
-for (let project of allProjects) {
   const wrapper = document.createElement("div");
   wrapper.classList.add("project__links");
   const liveLink = document.createElement("a");
@@ -58,13 +60,9 @@ for (let project of allProjects) {
     repoLink.textContent = "GitHub";
     wrapper.appendChild(repoLink);
   }
-  document
-    .querySelector(`#${project.camelCasedName}Wrapper`)
-    .appendChild(wrapper);
-}
+  infoWrapper.appendChild(wrapper);
 
 //add description for each project
-for (let project of allProjects) {
   const ul = document.createElement("ul");
   ul.classList.add("project__description-list");
   const points = project.description.split(".");
@@ -77,5 +75,5 @@ for (let project of allProjects) {
     li.textContent = point + (point === points[points.length - 1] ? "." : ";");
     ul.appendChild(li);
   }
-  document.querySelector(`#${project.camelCasedName}Wrapper`).appendChild(ul);
+  descriptionWrapper.appendChild(ul);
 }
